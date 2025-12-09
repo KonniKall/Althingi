90e9a8047361102aab03a6c2b521aa207a03dbb5
siljaros1994 <70152073+siljaros1994@users.noreply.github.com>
2025-12-09 17:17:34 +0000

    update

---
+++ Files/cert.sh
@@ -0,0 +1,6 @@
+## https://crt.sh 
+# %.althingi.is
+
+22927460004	2025-12-02	2025-12-02	2026-03-02	vod.althingi.is	vod.althingi.is	C=US, O=Let's Encrypt, CN=R12
+
+22141092731	2025-11-01	2025-11-01	2026-01-30	akstur.althingi.is	akstur.althingi.is	C=US, O=Let's Encrypt, CN=E8
